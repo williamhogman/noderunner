6520b71daf2dc9506101fb0e21bf47e39133b542
William Hogman <me@whn.se>
2012-10-01 16:55:09 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="noderunner",
+      description="NodeJS interoperability module",
+      version="0.0.1",
+      author="William Hogman",
+      author_email="me@whn.se",
+      packages=["noderunner"],
+      url="http://whn.se/noderunner",
+      package_dir={"noderunner": "noderunner"},
+      package_data={"noderunner": ["js/*.js"]},
+      requires=["gevent"],
+)
