ef8cfdac9336c9084c6c82e8a73520b1bce90546
William Hogman <me@whn.se>
2012-10-01 19:15:27 +0200

    added small repl for trying out NodeRunner

--- noderunner/__main__.py
+++ noderunner/__main__.py
@@ -3,8 +3,13 @@ import noderunner
 
 def main():
     client = noderunner.Client()
-    ret = client.eval("10+10")
-    print(ret)
+    print("NodeRunner REPL")
+    while True:
+        inp = raw_input(">")
+        if inp.strip() == "!quit":
+            break
+        ret = client.eval(inp)
+        print(ret)
 
 if __name__ == "__main__":
     main()
