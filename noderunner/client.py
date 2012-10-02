e5d8a1e7cdaa0fe695f320d6c96360cbb4e876bd
William Hogman <me@whn.se>
2012-10-02 22:01:19 +0200

    updated documentation for client

--- noderunner/client.py
+++ noderunner/client.py
@@ -6,8 +6,14 @@ from noderunner.socket import get_sockets
 
 
 class Client(object):
+    """ Primary class for interfacing with node
+
+    The client class provides an easy to use interface for calling
+    node functions, handling context and requirements.
+    """
 
     def __init__(self, secured=False):
+        """Spawns a new node process and sets up communication"""
         self._secured = secured
         self._start()
 
@@ -24,13 +30,30 @@ class Client(object):
         self._proto.start()
 
     def eval(self, code, context=None):
+        """Evaluates the code and returns the result
+
+        Evaluates the passed in string as JavaScript code. This code
+        may optionally be run in a context.
+
+        :return: The result of evaluating the expression, as best
+                 represented in python.
+        """
+
         return self._proto.request_sync("eval", code=code, context=context)
 
     def stop(self):
+        """Stops the client and terminates the node process"""
         self._proto.stop()
         self._proc.terminate()
 
     def context(self, name, reqs=[]):
+        """Creates a named context
+
+        Creates a named context with the passed in requirements
+        pre-included, the context will have it's own global object
+        making it hard to interfere with other things running in node.
+
+        :return: a context object for interfacing with it"""
         name = self._proto.request_sync("mkcontext",
                                         name=name,
                                         requirements=reqs)
